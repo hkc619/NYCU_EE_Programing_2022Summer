7173d1c2cc4b994ba363401c2dd2e6966a7902e6
Kyle Chiu <126474232+hkc619@users.noreply.github.com>
2023-04-17 01:41:15 +0800

    Add files via upload

---
+++ 20220707hw/20220707hw/20220707hw/20220707HW.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n = 1000000;
+	double sum  ;
+	sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += pow((double)i, 0.5) / pow((double)n, 1.5);
+	}
+	printf("%f", sum);
+}
+
+
