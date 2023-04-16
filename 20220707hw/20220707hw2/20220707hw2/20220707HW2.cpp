ba53c9a85a902101885ba22b5a294674acf9faca
Kyle Chiu <126474232+hkc619@users.noreply.github.com>
2023-04-17 01:42:10 +0800

    Add files via upload

---
+++ 20220707hw/20220707hw2/20220707hw2/20220707HW2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#define N 10
+
+int main()
+{
+    int A[N] = { 80,56,90,73,22,99,44,55,77,90 },temp;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++) 
+        {
+            if (A[i] > A[j])//(A[i] < A[j]由小排到大
+            {
+                temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+            printf("%d ", A[j]);
+        }
+        printf("\n");
+    }
+    /*
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i+1; j < N; j++) 由小排到大
+        {
+            if (A[i] > A[j])
+            {
+                temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+    }
+    */
+    
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("A[%d]=%d\n", i,A[i]);
+    }
+}
+
+
